b3d9d7e9929c7b50f54cb1c5fdba6a8dca9cf32c
tpischasova-str <tpischasova@setronica.com>
2021-07-14 11:03:20 +0700

    TSAPPS-220 [hotfix] Build productImport report with columns from mapping
    ProductImport report use hardcoded columns. It breaks Validation 2 scenario when custom mapping is defined

--- productImport/mapping/mapping.go
+++ productImport/mapping/mapping.go
@@ -7,12 +7,21 @@ import (
 	"os"
 )
 
+const (
+	categoryKey  = "Category" // TS min required column
+	productIdKey = "ID"       // TS min required
+	nameKey      = "Name"
+)
+
 type mapping struct {
-	Map map[string]string
+	rawMap    map[string]string
+	parsedMap *ColumnMap
 }
 
-func NewMappingHandler() MappingHandlerInterface {
-	return &mapping{}
+func NewMappingHandler(deps Deps) MappingHandlerInterface {
+	rawMap := mapping{}
+	rawMap.Init(deps.Config.ProductCatalog.MappingPath)
+	return &rawMap
 }
 
 func (m *mapping) Init(path string) map[string]string {
@@ -35,10 +44,32 @@ func (m *mapping) upload(mappingConfigPath string) {
 	if err := yaml.Unmarshal(data, rawMapping); err != nil {
 		log.Fatalf("unable to unmarshal mapping file %s\n%s", mappingConfigPath, err)
 	}
-
-	m.Map = rawMapping.ToConfig()
+	m.rawMap = rawMapping.ToConfig()
 }
 
 func (m *mapping) Get() map[string]string {
-	return m.Map
+	return m.rawMap
+}
+
+func (m *mapping) Parse() *ColumnMap {
+	rawMap := m.Get()
+	parsedMap := ColumnMap{}
+	if rawMap[categoryKey] != "" {
+		parsedMap.Category = rawMap[categoryKey]
+	} else {
+		parsedMap.Category = categoryKey
+	}
+
+	if rawMap[productIdKey] != "" {
+		parsedMap.ProductID = rawMap[productIdKey]
+	} else {
+		parsedMap.ProductID = productIdKey
+	}
+
+	if rawMap[nameKey] != "" {
+		parsedMap.Name = rawMap[nameKey]
+	} else {
+		parsedMap.Name = nameKey
+	}
+	return &parsedMap
 }
---
+++ productImport/mapping/mapping_test.go
@@ -0,0 +1,56 @@
+package mapping
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mapping_Parse(t *testing.T) {
+	type fields struct {
+		rawMap    map[string]string
+		parsedMap *ColumnMap
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *ColumnMap
+	}{
+		{
+			name: "positive: map should be converted to object with ProductID, Category and Name from relative columns",
+			fields: fields{
+				rawMap: map[string]string{
+					"ID":       "Label1",
+					"Category": "Label2",
+					"Name":     "Label3",
+				},
+			},
+			want: &ColumnMap{
+				ProductID: "Label1",
+				Category:  "Label2",
+				Name:      "Label3",
+			},
+		},
+		{
+			name: "positive: empty map should be converted to MAp Object with default values of ProductID, Category and Name",
+			fields: fields{
+				rawMap: nil,
+			},
+			want: &ColumnMap{
+				ProductID: "ID",
+				Category:  "Category",
+				Name:      "Name",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &mapping{
+				rawMap:    tt.fields.rawMap,
+				parsedMap: tt.fields.parsedMap,
+			}
+			if got := m.Parse(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
