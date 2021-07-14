edb3fed6cf086d0b8f9a54f9cbc6925c3e85ebf2
tpischasova-str <tpischasova@setronica.com>
2021-07-14 11:03:20 +0700

    TSAPPS-220 [hotfix] Build productImport report with columns from mapping
    ProductImport report use hardcoded columns. It breaks Validation 2 scenario when custom mapping is defined

--- productImport/mapping/handlerInterface.go
+++ productImport/mapping/handlerInterface.go
@@ -1,6 +1,23 @@
 package mapping
 
+import (
+	"go.uber.org/dig"
+	"ts/config"
+)
+
+type ColumnMap struct {
+	Category  string
+	ProductID string
+	Name      string
+}
+
 type MappingHandlerInterface interface {
 	Init(mappingConfigPath string) map[string]string
 	Get() map[string]string
+	Parse() *ColumnMap
+}
+
+type Deps struct {
+	dig.In
+	Config *config.Config
 }
